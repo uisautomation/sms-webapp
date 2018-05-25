8e75c8bd95c6da4499aa1317c642e45573b16fff
Abraham Martin <amc203@cam.ac.uk>
2018-05-25 16:20:07 +0100

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+python manage.py migrate                  # Apply database migrations
+python manage.py collectstatic --noinput  # Collect static files
+
+# Use gunicorn as a web-server after running migration command
+CMD gunicorn \
+	--name smswebapp \
+	--bind :$PORT \
+	--workers 3 \
+	--log-level=info \
+	--log-file=- \
+	--access-logfile=- \
+	--capture-output \
+	smswebapp.wsgi
