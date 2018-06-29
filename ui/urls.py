25eed0c3844908fca557217b6e93f374d3c49a6f
Mike Bamford <mb2174@cam.ac.uk>
2018-06-29 15:06:45 +0100

    added an "about" page

--- ui/urls.py
+++ ui/urls.py
@@ -14,6 +14,7 @@ You can use the default mapping by adding the following to your global ``urlpatt
 
 """
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -21,4 +22,5 @@ app_name = 'ui'
 
 urlpatterns = [
     path('media/<media_key>', views.media, name='media'),
+    path('about', TemplateView.as_view(template_name="ui/about.html"), name='about'),
 ]
