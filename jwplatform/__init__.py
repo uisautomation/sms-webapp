2ba9ff82b60244517d5f6a85c35c7c48657c9489
Rich Wareham <rjw57@cam.ac.uk>
2018-07-10 13:18:35 +0100

    add new jwplatform application
    Add a new application which will handle the jwplatform integration as
    per #129.

---
+++ jwplatform/__init__.py
@@ -0,0 +1,5 @@
+"""
+JWPlatform integration
+
+"""
+default_app_config = 'jwplatform.apps.Config'
---
+++ jwplatform/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class Config(AppConfig):
+    """Configuration for JWPlatform integration application."""
+    #: The short name for this application.
+    name = 'jwplatform'
+
+    #: The human-readable verbose name for this application.
+    verbose_name = 'JWPlatform'
