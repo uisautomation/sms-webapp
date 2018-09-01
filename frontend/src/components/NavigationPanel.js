4385bff080b5388e550dd4c978eb127eac531e2f
Rich Wareham <rich@richwareham.com>
2018-09-01 11:43:39 +0100

    frontend: NavigationPanel: implement feedback

--- frontend/src/components/NavigationPanel.js
+++ frontend/src/components/NavigationPanel.js
@@ -12,10 +12,11 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
-import CreateChannelIcon from '@material-ui/icons/AddToQueue';
+import CreateChannelIcon from '@material-ui/icons/LibraryAdd';
 import ChannelIcon from '@material-ui/icons/VideoLibrary';
 import LatestMediaIcon from '@material-ui/icons/NewReleases';
 import HomeIcon from '@material-ui/icons/Home';
@@ -23,6 +24,10 @@ import PopularIcon from '@material-ui/icons/TrendingUp';
 import FeedbackIcon from '@material-ui/icons/Feedback';
 import HelpIcon from '@material-ui/icons/Help';
 import SettingsIcon from '@material-ui/icons/Settings';
+import DropDownIcon from '@material-ui/icons/ArrowDropDown';
+import SignOutIcon from '@material-ui/icons/ExitToApp';
+import ShowMoreIcon from '@material-ui/icons/KeyboardArrowDown';
+import UploadIcon from '@material-ui/icons/CloudUpload';
 
 /** Side panel for the current user providing navigation links. */
 const NavigationPanel = ({ profile, classes }) => <div className={ classes.root }>
@@ -39,7 +44,11 @@ const NavigationPanel = ({ profile, classes }) => <div className={ classes.root 
         { profile.avatarImageUrl ? null : profile.displayName[0] }
       </Avatar>
       <Typography variant='title'>{ profile.displayName }</Typography>
-      <Typography variant='caption'>{ profile.username }</Typography>
+      <div className={ classes.profileUsernameContainer }>
+        <Typography variant='caption' className={ classes.profileUsername }>
+          { profile.username }
+        </Typography>
+      </div>
     </div>
     :
     null
@@ -65,43 +74,53 @@ const NavigationPanel = ({ profile, classes }) => <div className={ classes.root 
         <ListItemIcon><HomeIcon /></ListItemIcon>
         <ListItemText primary="Home" />
       </ListItem>
-      <ListItem button component={Link} to='/'>
-        <ListItemIcon><PopularIcon /></ListItemIcon>
-        <ListItemText primary="Popular" />
+      <ListItem button component={Link} to='/upload'>
+        <ListItemIcon><UploadIcon /></ListItemIcon>
+        <ListItemText primary="Upload" />
       </ListItem>
     </List>
     <Divider />
     <List subheader={<ListSubheader>Channels</ListSubheader>}>
       <ListItem button>
-        <ListItemAvatar><Avatar>G</Avatar></ListItemAvatar>
+        <ListItemIcon><CreateChannelIcon /></ListItemIcon>
         <ListItemText classes={{ root: classes.listItemRoot }}
-          secondary="47 items" primary="Philosophers of Greece" />
+          primary="Create channel" />
       </ListItem>
       <ListItem button>
-        <ListItemAvatar><Avatar src="https://randomuser.me/api/portraits/thumb/women/65.jpg" /></ListItemAvatar>
+        <ListItemAvatar><Avatar className={ classes.channelAvatar }>P</Avatar></ListItemAvatar>
+        <ListItemText classes={{ root: classes.listItemRoot }}
+          secondary="47 items" primary="Philosophers of Ancient Greece" />
+      </ListItem>
+      <ListItem button>
+        <ListItemAvatar><Avatar className={ classes.channelAvatar } src="https://randomuser.me/api/portraits/thumb/women/65.jpg" /></ListItemAvatar>
         <ListItemText classes={{ root: classes.listItemRoot }}
           secondary="1 item" primary="Smith Lectures" />
       </ListItem>
       <ListItem button>
-        <ListItemAvatar><Avatar
+        <ListItemAvatar><Avatar className={ classes.channelAvatar }
 src="http://www.sjcchoir.co.uk/sites/default/files/styles/main_content_small_image/public/images/125_col_small_st_johns_choir_17x15_c_b_ealovega.jpg?itok=8Y3mjZ1I" /></ListItemAvatar>
         <ListItemText classes={{ root: classes.listItemRoot }} 
           secondary="341 items" primary="St John's Choir" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon><ShowMoreIcon /></ListItemIcon>
+        <ListItemText classes={{ root: classes.listItemRoot }}
+          primary="Show more" />
       </ListItem>
     </List>
     <Divider />
     <List>
       <ListItem button>
-        <ListItemIcon><ChannelIcon /></ListItemIcon>
-        <ListItemText primary="Manage channels" />
+        <ListItemIcon><FeedbackIcon /></ListItemIcon>
+        <ListItemText primary="Feedback" />
       </ListItem>
       <ListItem button>
         <ListItemIcon><HelpIcon /></ListItemIcon>
         <ListItemText primary="Help" />
       </ListItem>
-      <ListItem button>
-        <ListItemIcon><FeedbackIcon /></ListItemIcon>
-        <ListItemText primary="Feedback" />
+      <ListItem button component='a' href='/accounts/logout'>
+        <ListItemIcon><SignOutIcon /></ListItemIcon>
+        <ListItemText primary="Sign out" />
       </ListItem>
     </List>
   </div>
@@ -111,13 +130,10 @@ src="http://www.sjcchoir.co.uk/sites/default/files/styles/main_content_small_ima
     <div className={classes.bottom}>
       <Divider />
       <div className={classes.bottomPanel}>
-        <Typography gutterBottom variant='caption'>
-          <a href="/account/logout">Sign out</a>
-        </Typography>
-        <Typography variant='caption'>
-          <a href="/about">About</a>{' '}
-          <a href="#">Contact</a>{' '}
-          <a href="https://github.com/uisautomation/sms-webapp">Developers</a>{' '}
+        <Typography variant='caption' gutterBottom className={ classes.bottomLinks }>
+          <a href="/about">About</a>
+          <a href="#">Contact</a>
+          <a href="https://github.com/uisautomation/sms-webapp">Developers</a>
         </Typography>
         <Typography variant='caption'>
           &copy; 2018 University of Cambridge
@@ -191,6 +207,23 @@ const styles = theme => ({
       textOverflow: 'ellipsis',
       whiteSpace: 'nowrap',
     },
+  },
+
+  bottomLinks: {
+    '& > a': {
+      marginRight: theme.spacing.unit,
+    },
+  },
+
+  profileUsernameContainer: {
+    display: 'flex',
+  },
+
+  profileUsername: {
+    flexGrow: 1,
+  },
+
+  channelAvatar: {
   },
 });
 
