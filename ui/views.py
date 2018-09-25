a4b36c8c4c72c561631a1fdbb24ff674fe0b373e
Rich Wareham <rjw57@cam.ac.uk>
2018-09-25 15:15:04 +0100

    ui: add RSS view of playlist
    Add a specialised view of a playlist as an RSS feed available under
    /playlists/<pk>.rss. We have to make sure to use the "slug" pattern in
    Django to match the playlist key as otherwise it greedily matches
    "foo.rss" as the playlist id.
    
    The specialist view is based on the playlist API view but with a custom
    renderer which uses feedgen to generate the RSS feed. The feed respects
    the "downloadable" property and makes sure that non-downloadable videos
    do not appear in the list.

--- ui/views.py
+++ ui/views.py
@@ -8,6 +8,8 @@ from rest_framework import generics
 from rest_framework.renderers import TemplateHTMLRenderer
 
 from api import views as apiviews
+
+from . import renderers
 from . import serializers
 
 LOG = logging.getLogger(__name__)
@@ -43,3 +45,17 @@ class PlaylistView(apiviews.PlaylistMixin, generics.RetrieveAPIView):
     serializer_class = serializers.PlaylistPageSerializer
     renderer_classes = [TemplateHTMLRenderer]
     template_name = 'ui/resource.html'
+
+
+class PlaylistRSSView(apiviews.PlaylistView):
+    """
+    Endpoint to retrieve an individual playlists as RSS. We specialise PlaylistView with a single
+    renderer class.
+
+    """
+    # We cannot simply make use of the normal DRF content negotiation and format_suffix_patterns()
+    # because this results in an additional "format" parameter being passed to the class which is
+    # then used to reverse() URLs for hyperlinked resources such as channels. Since none of those
+    # views support the format parameter, the reverse() call fails.
+
+    renderer_classes = [renderers.RSSRenderer]
